449bd4e2e9e705f7f7083414eddfcac605a9173d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-30 15:22:31 -0400

    setup: payments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200729114533) do
+ActiveRecord::Schema.define(version: 20200730191416) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
