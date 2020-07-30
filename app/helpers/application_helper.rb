077dc87704f5573f810a0f798819697d025c857a
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-30 11:26:54 -0400

    feature: session links at navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     case type
     when :notice
       %{
-        <div class="alert alert-success alert-dismissible fade show mb-5" role="alert">
+        <div class="alert alert-success alert-dismissible fade show mb-4" role="alert">
           #{content}
           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
@@ -12,12 +12,25 @@ module ApplicationHelper
       }.html_safe
     when :alert
       %{
-        <div class="alert alert-danger alert-dismissible fade show mb-5" role="alert">
+        <div class="alert alert-danger alert-dismissible fade show mb-4" role="alert">
           #{content}
           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
+      }.html_safe
+    end
+  end
+
+  def render_session_buttons
+    if user_signed_in?
+      %{
+        #{button_to 'Logout', destroy_user_session_path, method: :delete, form_class: 'form-inline my-2 my-lg-0', class: 'btn btn-sm btn-danger'}
+      }.html_safe
+    else
+      %{
+        #{button_to 'Login', new_user_session_path, method: :get, form_class: 'form-inline my-2 my-lg-0 mr-lg-2', class: 'btn btn-sm btn-success'}
+        #{button_to 'Sign up', new_user_registration_path, method: :get, form_class: 'form-inline my-2 my-lg-0', class: 'btn btn-sm btn-primary'}
       }.html_safe
     end
   end
