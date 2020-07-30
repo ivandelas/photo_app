a6d2f785ad70fd5fa1ac149c1ed9b9d7c191e6cc
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-30 10:28:00 -0400

    feature: messages partial, email configurations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def render_message(type, content)
+    case type
+    when :notice
+      %{
+        <div class="alert alert-success alert-dismissible fade show mb-5" role="alert">
+          #{content}
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      }.html_safe
+    when :alert
+      %{
+        <div class="alert alert-danger alert-dismissible fade show mb-5" role="alert">
+          #{content}
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      }.html_safe
+    end
+  end
 end
