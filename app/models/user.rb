4d5c0516da3909894aabe9236702cfb0188a07dc
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-06 20:03:35 -0400

    fix: username as heading

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
   has_many :orders
+
+  def email_username
+    self.email.split('@').first
+  end
 end
